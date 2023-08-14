f65a45d9d2dd58baefcd843a32af910aad4a038f
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2023-08-14 16:19:19 +0900

    #16 refactor transaction manger

--- usecase/interactor/archive.go
+++ usecase/interactor/archive.go
@@ -1,12 +1,12 @@
-package archive
+package interactor
 
 import (
 	"context"
 	"fmt"
 	"github.com/CityBear3/satellite/domain/model"
-	"github.com/CityBear3/satellite/logic"
-	"github.com/CityBear3/satellite/logic/dto"
 	"github.com/CityBear3/satellite/pkg/apperrs"
+	"github.com/CityBear3/satellite/usecase"
+	"github.com/CityBear3/satellite/usecase/dto"
 	"strings"
 )
 
@@ -14,21 +14,21 @@ const (
 	FileSizeLimitation = 8e6
 )
 
-type UploadArchiveInteractor struct {
+type ArchiveInteractor struct {
 	archiveRepository model.IArchiveRepository
-	txManager         logic.ITxManager
+	txManager         usecase.ITxManager
 }
 
-func NewArchiveImageInteractor(archiveRepository model.IArchiveRepository, txManager logic.ITxManager) *UploadArchiveInteractor {
-	return &UploadArchiveInteractor{
+func NewArchiveInteractor(archiveRepository model.IArchiveRepository, txManager usecase.ITxManager) *ArchiveInteractor {
+	return &ArchiveInteractor{
 		archiveRepository: archiveRepository,
 		txManager:         txManager,
 	}
 }
 
-// Handle do operation for upload image
-func (i *UploadArchiveInteractor) Handle(ctx context.Context, request dto.UploadArchiveRequest) error {
-	if err := i.txManager.DoInTx(ctx, func(ctxWithTx context.Context) error {
+// UploadArchive do operation for upload image
+func (i *ArchiveInteractor) UploadArchive(ctx context.Context, request dto.UploadArchiveRequest) error {
+	if _, err := i.txManager.DoInTx(ctx, func(ctxWithTx context.Context) (dto.IResult, error) {
 		archive := model.Archive{
 			Id:       request.Id,
 			Size:     len(request.Data),
@@ -36,12 +36,12 @@ func (i *UploadArchiveInteractor) Handle(ctx context.Context, request dto.Upload
 			DeviceId: request.DeviceId,
 		}
 		if !archive.CheckDataSize(FileSizeLimitation) {
-			return apperrs.NewError(apperrs.BadRequest, fmt.Sprintf(apperrs.InvalidFileSizeMSG, "8MB"))
+			return nil, apperrs.NewError(apperrs.BadRequest, fmt.Sprintf(apperrs.InvalidFileSizeMSG, "8MB"))
 		}
 		if err := i.archiveRepository.Save(ctxWithTx, archive); err != nil {
-			return err
+			return nil, err
 		}
-		return nil
+		return nil, nil
 	}); err != nil {
 		return err
 	}
